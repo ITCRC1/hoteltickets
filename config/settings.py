eae328e7dabb1a58d201c3e5cc0b52f2b3ddc7a3
ITCRC1 <it@thecostaricacollection.com>
2026-06-02 15:08:37 -0600

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -89,6 +89,7 @@ STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_DIRS = [BASE_DIR / 'static']
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
+WHITENOISE_USE_FINDERS = True
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
